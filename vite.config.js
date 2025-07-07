4afba4a00dc74e248fdd5254693c15f7d466043f
Nihat Engel <nihat.engel@mirascapital.com>
2025-07-07 12:41:29 +1000

    Deploy build output to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Curve/', // Set this to match your repository name
-})
+  base: '/curve/', // Ensure this matches your repository name (case-sensitive)
+});
