929300ad55b8903b6824e35026d7499852ff4304
MIRAS CAPITAL <154988581+MirasCapital@users.noreply.github.com>
2025-07-10 01:10:14 +0000

    vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/curve/',
   plugins: [react()],
-  base: '/curve/', // This line should be here
 });
